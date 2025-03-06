eae1f8b977129b012a6d569bd27ed5645aebc4cc
Aishwaryajannu <aishwaryajannu17@gmail.com>
2025-03-06 22:51:31 +0530

    Fixed views

--- api/views.py
+++ api/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
+import logging
+logger = logging.getLogger(__name__)
 
 class JobApplicationListCreate(generics.ListCreateAPIView):
     queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
+
+    def create(self, request, *args, **kwargs):
+        logger.info("Received Data: %s", request.data)  # Log request data
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        logger.error("Validation Errors: %s", serializer.errors)  # Log errors
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.

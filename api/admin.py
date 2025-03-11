0f4e44edacaf2d0eb1331cd865e8dfe069cdc334
Aishwaryajannu <aishwaryajannu17@gmail.com>
2025-03-11 11:13:54 +0530

    Added admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import JobApplication
+from django.utils.html import format_html
+from .models import JobApplication 
+from django.contrib.auth.models import Group, User # Import your JobApplication model
 
-@admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'job_role', 'address', 'city', 'pincode', 'date')
+    list_filter = ('city', 'job_role')  # Optional: Filtering options
+    search_fields = ('first_name', 'last_name', 'email', 'job_role', 'city')  # Add search functionality
 
-# Register your models here.
+    def changelist_view(self, request, extra_context=None):
+        extra_context = {'title': 'Manage Job Applications'}  # Change title
+        return super().changelist_view(request, extra_context=extra_context)
+
+# Register the model
+admin.site.register(JobApplication, JobApplicationAdmin)
+
+# Hide all other sections in the admin panel
+admin.site.site_header = "Job Applications Admin"
+admin.site.index_title = ""  # Remove the extra sections
+admin.site.site_title = "Job Applications"
